02897533ddabdee6ba262244761730b518257f7a
Yevhen <benatanlev@icloud.com>
2025-05-03 20:08:47 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import s from "./App.module.css";
 
-import Container from "./components/Container/Container";
-
 function App() {
   return (
-    <Container>
+    <>
       <h1 className={s.title}>Phonebook</h1>
       {/* <ContactForm />
       <SearchBox />
       <ContactList /> */}
-    </Container>
+    </>
   );
 }
 
 export default App;
+
