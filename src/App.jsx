38a17485bc8a162a2f56269aa9267209ded3d46f
Yevhen <benatanlev@icloud.com>
2025-05-03 20:14:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import s from "./App.module.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Container from "./components/Container/Container";
 
 function App() {
   return (
-    <>
+    <Container>
       <h1 className={s.title}>Phonebook</h1>
-      {/* <ContactForm />
-      <SearchBox />
-      <ContactList /> */}
-    </>
+      <ContactForm />
+      {/* <SearchBox /> */}
+      {/* <ContactList /> */}
+    </Container>
   );
 }
 
 export default App;
-
